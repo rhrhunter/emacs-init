f431b5bc943fda5fbf5cb75dfebcb3f4c39a2b4e
Robert Hunter <rhrhunter@users.noreply.github.com>
2024-06-30 08:04:41 -0400

    Update 03_mark_buffers.el

---
+++ custom-lisp/03_mark_buffers.el
@@ -0,0 +1,3 @@
+;; [Ctrl+c -> TAB] -- Mark the entire file
+;; Hint: Useful for auto-formatting the entire file by pressing (Ctrl+c -> TAB -> TAB)
+(global-set-key (kbd "C-c TAB") 'mark-whole-buffer)
