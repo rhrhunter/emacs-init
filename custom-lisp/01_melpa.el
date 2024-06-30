4bb8cca6a2774b600106e46397632d0175ef25fb
Robert Hunter <rhrhunter@users.noreply.github.com>
2024-06-30 07:57:27 -0400

    Update 01_melpa.el

--- custom-lisp/01_melpa.el
+++ custom-lisp/01_melpa.el
@@ -1,3 +1,11 @@
 ;; Register the Melpa Emacs Package Repository
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; [Ctrl+c -> i] -- Open the Melpa Emacs Package Screen
+(defun func/open-package-installer ()
+  (interactive)
+  (package-refresh-contents)
+  (package-list-packages)
+  )
+(global-set-key (kbd "C-c i") 'func/open-package-installer)
