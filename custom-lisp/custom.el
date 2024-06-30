58c0a6b3e722bfb91dcf8dc7981a86dd629cb309
Robert Hunter <rhrhunter@users.noreply.github.com>
2024-06-30 08:10:05 -0400

    Update custom.el

--- custom-lisp/custom.el
+++ custom-lisp/custom.el
@@ -1,24 +1,6 @@
 
-
-
-
-;; Make all "yes or no" prompts show "y or n" instead
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; Make it so all '.js' files auto load 'rjsx-mode'
-(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
-
 ;; Disable tab characters in indentation
 (setq-default indent-tabs-mode nil)
 
 ;; Use 4 spaces when tabbing HTML elements
 (setq-default sgml-basic-offset 4)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Custom Key Bindings ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; Ctrl+c key bindings
-
