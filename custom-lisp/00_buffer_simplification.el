941f699a11a11d2c66a77f1a0c78f655c6e0bf02
Robert Hunter <rhrhunter@users.noreply.github.com>
2024-06-30 08:06:52 -0400

    Update 00_buffer_simplification.el

---
+++ custom-lisp/00_buffer_simplification.el
@@ -0,0 +1,10 @@
+;; Disable the *Messages* Buffer
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+;; Disable the *Completions* buffer
+(add-hook 'minibuffer-exit-hook 
+          '(lambda ()
+             (let ((buffer "*Completions*"))
+               (and (get-buffer buffer)
+                    (kill-buffer buffer)))))
